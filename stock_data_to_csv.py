ab3c028fdc95903c4e58b61df63453337395bba9
Gareth Jones <garethjones@cybertron.local>
2018-07-01 13:19:52 +0100

    converted dataframe and saved it as a csv file

--- stock_data_to_csv.py
+++ stock_data_to_csv.py
@@ -6,11 +6,13 @@ import pandas_datareader.data as web
 
 style.use('ggplot')
 
-start = dt.datetime(2013,1,1)
-end = dt.datetime.now()
+start = dt.datetime(2014,1,1)
+end = dt.datetime(2015,1,1)
 
 df = web.DataReader('NFLX', 'morningstar', start, end) #dataframe
 df.reset_index(inplace=True)
 df.set_index("Date", inplace=True)
 df = df.drop("Symbol", axis=1)
-print(df.head(10))
+#print(df.head(100))
+
+df.to_csv('nflx.csv')
