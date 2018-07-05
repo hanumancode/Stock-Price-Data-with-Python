88ac7688affbbd93b1e5d07fbb5a94152571844a
Gareth Jones <garethjones@cybertron.local>
2018-07-05 19:10:20 +0100

    added a moving average to the price data on the first axis and volume on axis 2

--- stock_data_read_csv.py
+++ stock_data_read_csv.py
@@ -7,7 +7,20 @@ import pandas_datareader.data as web
 style.use('ggplot')
 
 df = pd.read_csv('nflx.csv', parse_dates=True, index_col=0) #dataframe
+
+df['21ma'] = df['Close'].rolling(window=21).mean()
+
+df.dropna(inplace=True) #modifies the dataframe in place
+
 #print(df.head())
 
-df['Close'].plot()
+#graph with matplotlib with the values from Pandas
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1) # 6 rows, 1 column
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1)
+
+ax1.plot(df.index, df['Close'])
+ax1.plot(df.index, df['21ma'])
+ax2.bar(df.index, df['Volume'])
+
 plt.show()
